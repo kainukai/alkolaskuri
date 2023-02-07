affa5d7044ab09be45055976fb1011bc8d5ee9f2
kainukai <117354315+kainukai@users.noreply.github.com>
2023-02-07 20:12:24 +0200

    Add files via upload
    Jospa nyt ois oikea koodi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [weight, setWeight] = useState("");
+  const [beer, setBeer] = useState("");
+  const [time, setTime] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const litres = beer * 0.33;
+    const grams = litres * 8 * 4.5;
+    const burning = weight / 10;
+    const gramsLeft = grams - (burning * time);
+    let alcohol; alcohol = gramsLeft / (weight * 0.7);
+
+    if (alcohol < 0) {
+      alcohol = 0;
+    }
+
+    setResult(alcohol.toFixed(1));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div>
+  <h1>Calculating alcohol blood level</h1>
+  <form onSubmit={handleSubmit}>
+
+    <div>
+    <label>Weight <input type="number" value={weight} onChange={e => setWeight(e.target.value)}/>
+    </label>
     </div>
+
+    <div>
+    <label>Bottles <input type="number" value={beer} onChange={e => setBeer(e.target.value)}/>
+    </label>
+    </div>
+
+    <div>
+    <label>Time <input type="number" value={time} onChange={e => setTime(e.target.value)}/>
+    </label>
+    </div>
+    
+    <h3>{result}</h3>
+        <button type="submit">Calculate</button>
+      </form>
+      </div>
   );
-}
+};
 
 export default App;
